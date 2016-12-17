8a63201886d64003d449827ca3e5d499c0a73551
ohbarye <over.rye@gmail.com>
2016-12-17 19:23:27 +0900

    Fix setup.sh to cp prezto files

--- setup.sh
+++ setup.sh
@@ -1,8 +1,22 @@
-#!/bin/bash
+#!/bin/zsh
 
 DOT_FILES=( .bash_profile .bashrc .zshrc .gitconfig .vimrc .vim .bundle .gitignore_global .tmux .tmux.conf )
 
 for file in ${DOT_FILES[@]}
 do
   ln -s $HOME/dotfiles/$file $HOME/$file
+done
+
+source $HOME/dotfiles/antigen/antigen.zsh
+
+#antigen bundle sorin-ionescu/prezto
+
+ln -s ~/.antigen/repos/.zprezto ~/.zprezto
+
+PREZTO_FILES=( zlogin zlogout zpreztorc zprofile zshenv zshrc )
+
+for file in ${PREZTO_FILES[@]}
+do
+  cp $HOME/.zprezto/runcoms/$file $HOME/dotfiles/.$file
+  ln -s $HOME/dotfiles/.$file $HOME/.$file
 done
